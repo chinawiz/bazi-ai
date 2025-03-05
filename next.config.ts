c076be4a04bfa93865eac856af3dd4cd4ac5ad9a
chinawiz <chinawiz@gmail.com>
2025-03-05 18:24:13 +0800

    删除了部署到github的代码；清理优化代码

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // 如果将来需要其他配置，可以在这里添加
 };
 
 export default nextConfig;
